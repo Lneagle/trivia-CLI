9443b522987c572993cef070de1bcb0580d1382e
Lacey Murphy <lneagle@gmail.com>
2025-07-17 20:10:12 -0400

    Got main functionality working, reconfigured questions

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,82 @@
 #!usr/bin/env node
 
 import { questions } from "../src/lib/Questions.js";
+/* let questions = [
+    {
+        question: "What is the term for a group of flamingos?",
+        options: ["Murder", "Flamboyance", "Herd", "Gaggle"],
+        answer: "Flamboyance"
+    }
+];*/
 
-let currentQuestion = questions.splice(Math.floor(Math.random() * questions.length), 1);
+import { select } from "@inquirer/prompts";
+import chalk from "chalk";
+
+let responses = [];
+
+async function displayQuestion() {
+    let currentQuestion = questions.splice(Math.floor(Math.random() * questions.length), 1);
+    currentQuestion = currentQuestion[0];
+    const userChoice = await select({ 
+        message: currentQuestion.question,
+        choices: currentQuestion.options.map((option) => ({ name: option, value: option }))
+    });
+    responses.push({questionText: currentQuestion.question, answer: currentQuestion.answer, correct: userChoice === currentQuestion.answer ? true : false});
+    if (questions.length > 0 && timer > 0) {
+        console.log(`${timer} seconds left!`);
+        displayQuestion();
+    } else {
+        if (timer > 0) {
+            console.log("No more questions!");
+            clearInterval(timerId);
+        } else {
+            console.log ("Time's up!");
+        }
+        displayFeedback();
+    }
+}
+
+function displayFeedback() {
+    console.log(`Great game! You answered ${responses.length} questions.  Let's see how you did.`);
+    let numCorrect = 0;
+    responses.forEach((response) => {
+        console.log(response.questionText);
+        if (response.correct) {
+            console.log(chalk.green(`Correct! The answer was ${response.answer}`));
+            numCorrect++;
+        } else {
+            console.log(chalk.red(`Sorry! The answer was ${response.answer}`));
+        }
+    });
+    console.log(`You got ${numCorrect} out of ${responses.length} correct!`)
+}
+
+let timer = 60;
+let timerId;
+
+const action = await select({
+    message: "Main Menu",
+    choices: [
+        { name: "Start Game", value: "start" },
+        { name: "Quit", value: "quit" }
+    ]
+});
+
+switch (action) {
+    case "start":
+        console.log("Welcome to Trivia CLI! Answer as many questions as you can in 1 minute.")
+        timerId = setInterval(() => {
+            if (timer > 0) {
+                timer--;
+            } else {
+                clearInterval(timerId);
+            }
+        }, 1000);
+        await displayQuestion();
+        break;
+    case "quit":
+        console.log("Goodbye!");
+        process.exit(0);
+}
+
+//program.parse(process.argv);
--- src/lib/Questions.js
+++ src/lib/Questions.js
@@ -1,212 +1,52 @@
 export const questions = [
     {
         question: "What is the term for a group of flamingos?",
-        options: [
-            {
-                text: "Murder",
-                correct: false
-            },
-            {
-                text: "Flamboyance",
-                correct: true
-            },
-            {
-                text: "Herd",
-                correct: false
-            },
-            {
-                text: "Gaggle",
-                correct: false
-            }
-        ]
+        options: ["Murder", "Flamboyance", "Herd", "Gaggle"],
+        answer: "Flamboyance"
     },
     {
         question: "What is the fear of clowns called?",
-        options: [
-            {
-                text: "Coulrophobia",
-                correct: true
-            },
-            {
-                text: "Triskaidekaphobia",
-                correct: false
-            },
-            {
-                text: "Arachnophobia",
-                correct: false
-            },
-            {
-                text: "Chromophobia",
-                correct: false,
-            }
-        ]
+        options: ["Coulrophobia", "Triskaidekaphobia", "Arachnophobia", "Chromophobia"],
+        answer: "Coulrophobia"
     },
     {
         question: "Which mountain range is the longest in the world?",
-        options: [
-            {
-                text: "The Rockies",
-                correct: false
-            },
-            {
-                text: "The Himalayas",
-                correct: false
-            },
-            {
-                text: "The Alps",
-                correct: false
-            },
-            {
-                text: "The Andes",
-                correct: true
-            }
-        ]
+        options: ["The Rockies", "The Himalayas", "The Alps", "The Andes"],
+        answer: "The Andes"
     },
     {
         question: "What does 'HTTP' stand for?",
-        options: [
-            {
-                text: "How To Train Pigeons",
-                correct: false
-            },
-            {
-                text: "High Tech Typing Program",
-                correct: false
-            },
-            {
-                text: "HyperText Transfer Protocol",
-                correct: true
-            },
-            {
-                text: "Hundred Tuba Tapdancing Program",
-                correct: false
-            }
-        ]
+        options: ["How To Train Pigeons", "High Tech Typing Program", "HyperText Transfer Protocol", "Hundred Tuba Tapdancing Program"],
+        answer: "HyperText Transfer Protocol"
     },
     {
         question: "What is the symbol for potassium?",
-        options: [
-            {
-                text: "K",
-                correct: true
-            },
-            {
-                text: "Pt",
-                correct: false
-            },
-            {
-                text: "P",
-                correct: false
-            },
-            {
-                text: "Sm",
-                correct: false
-            }
-        ]
+        options: ["K", "Pt", "P", "Sm"],
+        answer: "K"
     },
     {
         question: "What is the name of Batman's butler?",
-        options: [
-            {
-                text: "Albert",
-                correct: false
-            },
-            {
-                text: "Alfred",
-                correct: true
-            },
-            {
-                text: "Robin",
-                correct: false
-            },
-            {
-                text: "Catwoman",
-                correct: false
-            }
-        ]
+        options: ["Albert", "Alfred", "Robin", "Catwoman"],
+        answer: "Alfred"
     },
     {
         question: "How many films did Sean Connery play James Bond in?",
-        options: [
-            {
-                text: "4",
-                correct: false
-            },
-            {
-                text: "6",
-                correct: false
-            },
-            {
-                text: "7",
-                correct: true
-            },
-            {
-                text: "9",
-                correct: false
-            }
-        ]
+        options: ["4", "6", "7", "9"],
+        answer: "7"
     },
     {
         question: "What is the longest-running Broadway show?",
-        options: [
-            {
-                text: "Cats",
-                correct: false
-            },
-            {
-                text: "The Music Man",
-                correct: false
-            },
-            {
-                text: "Hamilton",
-                correct: false
-            },
-            {
-                text: "The Phantom of the Opera",
-                correct: true
-            }
-        ]
+        options: ["Cats", "The Music Man", "Hamilton", "The Phantom of the Opera"],
+        answer: "The Phantom of the Opera"
     },
     {
         question: "Which hockey team did Wayne Gretzky play for in the '80s?",
-        options: [
-            {
-                text: "Edmonton Oilers",
-                correct: true
-            },
-            {
-                text: "New Jersey Devils",
-                correct: false
-            },
-            {
-                text: "Pittsburgh Penguins",
-                correct: false
-            },
-            {
-                text: "Winnipeg Jets",
-                correct: false
-            }
-        ]
+        options: ["Edmonton Oilers", "New Jersey Devils", "Pittsburgh Penguins", "Winnipeg Jets"],
+        answer: "Edmonton Oilers"
     },
     {
         question: "What's the primary ingredient in hummus?",
-        options: [
-            {
-                text: "Tahini",
-                correct: false
-            },
-            {
-                text: "Chickpeas",
-                correct: true
-            },
-            {
-                text: "Onions",
-                correct: false
-            },
-            {
-                text: "Chocolate",
-                correct: false
-            }
-        ]
+        options: ["Tahini", "Chickpeas", "Onions", "Chocolate"],
+        answer: "Chickpeas"
     }
 ]
